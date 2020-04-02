5c527efadc3e41c816f08abe390ac582583c4bbf
jsonhyun <airplant@naver.com>
2020-04-02 11:16:25 +0900

    매퍼 xml, 일대다 매핑

---
+++ src/test/java/mybatis_study/mappers/TutorMapperTest.java
@@ -0,0 +1,51 @@
+package mybatis_study.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import mybatis_study.dto.Course;
+import mybatis_study.dto.Tutor;
+import mybatis_study.jdbc.AbstractTest;
+import mybatis_study.jdbc.MyBatisSqlSessionFactory;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TutorMapperTest extends AbstractTest{
+	private static TutorMapperImpl dao;
+	private static SqlSession sqlSession;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		dao = TutorMapperImpl.getInstance();
+		sqlSession = MyBatisSqlSessionFactory.openSession();
+        dao.setSqlSession(sqlSession);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		dao = null;
+		sqlSession.close();
+	}
+
+	@Test
+	public void test01SelectTutorByTutorId() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName()+"()");
+		Tutor findTutor = new Tutor();
+		findTutor.setTutorId(1);
+		Tutor tutor = dao.selectTutorByTutorId(findTutor);
+		Assert.assertEquals(tutor.getTutorId(), findTutor.getTutorId());
+		
+		log.trace(tutor.getTutorId() + " : " + tutor.getTutorId());
+		List<Course> courses = tutor.getCourses();
+		for(Course c : courses) {
+			log.trace(c.toString());
+		}
+	}
+
+}
