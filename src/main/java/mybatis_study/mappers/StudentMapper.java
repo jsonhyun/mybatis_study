e1344016933bc73b5705d361409d0112d9dc1f43
jsonhyun <airplant@naver.com>
2020-04-06 09:03:45 +0900

    여러개의 입력 파라미터, ResultSet의 재정의

--- src/main/java/mybatis_study/mappers/StudentMapper.java
+++ src/main/java/mybatis_study/mappers/StudentMapper.java
@@ -12,12 +12,16 @@ public interface StudentMapper {
 	Student selectStudentByNOWithResultMap(Student student);
 	
 	Student selectStudentByNOAssociation(Student student);
+	// 여러 개의 입력 파라미터 전달
+	Student selectAllStudentByMap(Map<String, String> map);
 	
 	List<Student> selectStudentByAll();
 	//ResultMap
 	List<Student> selectStudentByAllForResultMap();
 	//hashMap
 	List<Map<String, Object>> selectStudentByAllForHashMap();
+	//ResultSet 처리방식의 재정의
+	Map<Integer, Student> selectStudentForMap(int studId);	
 	
 	int insertStudent(Student student);
 	int deleteStudent(int id);
--- src/main/java/mybatis_study/mappers/StudentMapperImpl.java
+++ src/main/java/mybatis_study/mappers/StudentMapperImpl.java
@@ -1,8 +1,11 @@
 package mybatis_study.mappers;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.session.ResultContext;
+import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.SqlSession;
 
 import mybatis_study.dto.Student;
@@ -71,6 +74,26 @@ public class StudentMapperImpl implements StudentMapper {
 	@Override
 	public int insertEnumStudent(Student student) {
 		return sqlSession.insert(namespace + ".insertEnumStudent", student);
+	}
+
+	@Override
+	public Student selectAllStudentByMap(Map<String, String> map) {
+		return sqlSession.selectOne(namespace+".selectAllStudentByMap", map);
+	}
+
+	@Override
+	public Map<Integer, Student> selectStudentForMap(int studId) {
+		Map<Integer, Student> map = new HashMap<Integer, Student>();
+		ResultHandler<Student> resultHandler = new ResultHandler<Student>() {
+
+			@Override
+			public void handleResult(ResultContext<? extends Student> resultContext) {
+				Student student = resultContext.getResultObject();
+				map.put(student.getStudId(), student);
+			}
+		};
+		sqlSession.select(namespace+".selectStudentForMap", studId, resultHandler);
+		return map;
 	}
 
 }
